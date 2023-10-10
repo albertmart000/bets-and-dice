5ed0acfd4977722d4a3210f25e66abe0fdd0b174
albertmart000 <albertmart000@yahoo.es>
2023-10-10 09:42:19 +0200

    Added Swagger

--- betsanddice-user/src/main/java/com/betsanddice/user/controller/UserController.java
+++ betsanddice-user/src/main/java/com/betsanddice/user/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.betsanddice.user.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/betsanddice/api/v1/user")
 public class UserController {
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
 }
--- betsanddice-user/src/main/java/com/betsanddice/user/service/UserServiceImp.java
+++ betsanddice-user/src/main/java/com/betsanddice/user/service/UserServiceImp.java
@@ -1,11 +1,15 @@
 package com.betsanddice.user.service;
 
 import com.betsanddice.user.repository.IUserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService implements  IUserService{
+public class UserServiceImp implements  IUserService{
+
+    private static final Logger log = LoggerFactory.getLogger(UserServiceImp.class);
 
     @Autowired
     private IUserRepository userRepository;
