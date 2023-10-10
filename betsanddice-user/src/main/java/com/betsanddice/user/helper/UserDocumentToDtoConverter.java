a8518ee462dc9a41639dda2cd84f8ea0e587d3fe
albertmart000 <albertmart000@yahoo.es>
2023-10-10 11:47:37 +0200

    Added @Component in converter

--- betsanddice-user/src/main/java/com/betsanddice/user/helper/UserDocumentToDtoConverter.java
+++ betsanddice-user/src/main/java/com/betsanddice/user/helper/UserDocumentToDtoConverter.java
@@ -7,11 +7,12 @@ import org.modelmapper.AbstractConverter;
 import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-
+@Component
 public class UserDocumentToDtoConverter{
 
     static final DateTimeFormatter CUSTOM_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
