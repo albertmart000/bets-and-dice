145245f2f25dfe6a75c6296208540de17652ead1
albertmart000 <albertmart000@yahoo.es>
2023-10-13 12:52:02 +0200

    Added ErrorResponseMessageTest

--- betsanddice-user/src/main/java/com/betsanddice/user/exception/GlobalExceptionHandler.java
+++ betsanddice-user/src/main/java/com/betsanddice/user/exception/GlobalExceptionHandler.java
@@ -1,15 +1,23 @@
 package com.betsanddice.user.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<ErrorResponseMessage> handleResponseStatusException(ResponseStatusException ex) {
+        HttpStatus statusCode = (HttpStatus) ex.getStatusCode();
+        ErrorResponseMessage errorResponseMessage = new ErrorResponseMessage(statusCode.value(), ex.getReason());
+        return ResponseEntity.status(statusCode).body(errorResponseMessage);
+    }
 
     @ExceptionHandler(BadUuidException.class)
     public ResponseEntity<ErrorResponseMessage> handleBadUUID(BadUuidException ex) {
