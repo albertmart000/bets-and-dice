624379ebbe1b072298ff7ef54523ecd490767a57
albertmart000 <albertmart000@yahoo.es>
2023-10-13 12:29:07 +0200

    Added GlobalExceptionHandler and annotations UserDto

---
+++ betsanddice-user/src/main/java/com/betsanddice/user/exception/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.betsanddice.user.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BadUuidException.class)
+    public ResponseEntity<ErrorResponseMessage> handleBadUUID(BadUuidException ex) {
+        return ResponseEntity.badRequest().body(new ErrorResponseMessage(ex.getMessage()));
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponseMessage> handleUserNotFoundException(UserNotFoundException ex) {
+        return ResponseEntity.badRequest().body(new ErrorResponseMessage(ex.getMessage()));
+    }
+    @ExceptionHandler(ConverterException.class)
+    public ResponseEntity<ErrorResponseMessage> handleConverterException(UserNotFoundException ex) {
+        return ResponseEntity.badRequest().body(new ErrorResponseMessage(ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseMessage> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(new ErrorResponseMessage("Parameter not valid", errors));
+    }
+
+}
+
+
+
