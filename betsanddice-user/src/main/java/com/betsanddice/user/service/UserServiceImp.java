7d3cb03d0090dbe516f6bb6d9da6b371406927f6
albertmart000 <albertmart000@yahoo.es>
2023-10-15 11:52:56 +0200

    Added getUserByUuid in service

--- betsanddice-user/src/main/java/com/betsanddice/user/service/UserServiceImp.java
+++ betsanddice-user/src/main/java/com/betsanddice/user/service/UserServiceImp.java
@@ -1,6 +1,8 @@
 package com.betsanddice.user.service;
 
 import com.betsanddice.user.dto.UserDto;
+import com.betsanddice.user.exception.UserNotFoundException;
+import com.betsanddice.user.helper.UserDocumentToDtoConverter;
 import com.betsanddice.user.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,22 +10,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.util.UUID;
+
+
 @Service
 public class UserServiceImp implements IUserService {
-
-    //conservar esta anotacion
 
     private static final Logger log = LoggerFactory.getLogger(UserServiceImp.class);
 
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private UserDocumentToDtoConverter converter;
+
     @Override
     public Mono<UserDto> getUserByUuid(String uuid) {
-        return null;
+        return Mono.just(UUID.fromString(uuid))
+                .flatMap(userUuid -> userRepository.findByUuid(userUuid)
+                        .map(user ->converter.fromDocumentToDto(user))
+                        .switchIfEmpty(Mono.error(new UserNotFoundException("User with id " + userUuid + " not found")))
+                        .doOnSuccess(userDto -> log.info("User found with ID: {}", userUuid))
+                        .doOnError(error -> log.error("Error occurred while retrieving user: {}", error.getMessage()))
+                );
     }
+    
 }
-
-
-
 
