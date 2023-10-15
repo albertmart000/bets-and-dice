c1259198544c3e33f478920c6a5d4cef7f3a3118
albertmart000 <albertmart000@yahoo.es>
2023-10-15 13:48:44 +0200

    Added findByUuidTest

--- betsanddice-user/src/test/java/com/betsanddice/user/repository/UserRepositoryTest.java
+++ betsanddice-user/src/test/java/com/betsanddice/user/repository/UserRepositoryTest.java
@@ -1,11 +1,23 @@
 package com.betsanddice.user.repository;
 
+import com.betsanddice.user.document.UserDocument;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.util.AssertionErrors.fail;
 
 @DataMongoTest
 class UserRepositoryTest {
@@ -13,11 +25,51 @@ class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
+    UUID uuidUser1 = UUID.fromString("81099a9e-0d59-4571-a04c-31a08a711e3b");
+    UUID uuidUser2 = UUID.fromString("26977eee-89f8-11ec-a8a3-0242ac120003");
+
+    @BeforeEach
+    public void setUp() {
+
+        userRepository.deleteAll().block();
+
+        UUID uuidGame1 = UUID.fromString("dcacb291-b4aa-4029-8e9b-284c8ca80296");
+        UUID uuidGame2 = UUID.fromString("09fabe32-7362-4bfb-ac05-b7bf854c6e0f");
+        List<UUID> gameList = List.of(uuidGame1, uuidGame2);
+
+        UUID uuidStatistics1 = UUID.fromString("70e9755e-9e83-41d3-853a-665f1f2a2f5c");
+        UUID uuidStatistics2 = UUID.fromString("bb7897b8-517d-4843-8c17-e347aba086ca");
+        List<UUID> statisticsList = List.of(uuidStatistics1, uuidStatistics2);
+
+        UserDocument user1 = new UserDocument(uuidUser1, "Morrow", "Montgomery", LocalDate.now(),
+                "Player2", "morrowmontgomery@email.com", "player2", LocalDateTime.now(),
+                BigDecimal.valueOf(100), gameList, statisticsList);
+
+        UserDocument user2 = new UserDocument(uuidUser2, "Morrow", "Montgomery", LocalDate.now(),
+                "Player2", "morrowmontgomery@email.com", "player2", LocalDateTime.now(),
+                BigDecimal.valueOf(100), gameList, statisticsList);
+
+        userRepository.saveAll(Flux.just(user1, user2)).blockLast();
+    }
+
     @DisplayName("Repository not null Test")
     @Test
     void testDB() {
         assertNotNull(userRepository);
-
     }
 
+    @DisplayName("Find by UUID Test")
+    @Test
+    void findByUuidTest() {
+
+        Mono<UserDocument> user1 = userRepository.findByUuid(uuidUser1);
+        user1.blockOptional().ifPresentOrElse(
+                user -> assertEquals(user.getUuid(), uuidUser1),
+                () -> fail("Challenge not found: " + uuidUser1));
+
+        Mono<UserDocument> user2 = userRepository.findByUuid(uuidUser2);
+        user2.blockOptional().ifPresentOrElse(
+                user -> assertEquals(user.getUuid(), uuidUser2),
+                () -> fail("Challenge not found: " + uuidUser2));
+    }
 }
