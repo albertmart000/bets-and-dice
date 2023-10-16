c71c931e74da362a8404ccbb4c00df91e8ddf5b0
albertmart000 <albertmart000@yahoo.es>
2023-10-16 12:00:34 +0200

    Fixed bug UserRepositoryTest

--- betsanddice-user/src/test/java/com/betsanddice/user/repository/UserRepositoryTest.java
+++ betsanddice-user/src/test/java/com/betsanddice/user/repository/UserRepositoryTest.java
@@ -4,31 +4,52 @@ import com.betsanddice.user.document.UserDocument;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
 import java.math.BigDecimal;
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.List;
+import java.util.UUID;
 
 import static org.junit.Assert.assertNotNull;
 
 @DataMongoTest
+@Testcontainers
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class UserRepositoryTest {
+
+    @Container
+    static MongoDBContainer container = new MongoDBContainer("mongo")
+            .withStartupTimeout(Duration.ofSeconds(60));
+
+    @DynamicPropertySource
+    static void initMongoProperties(DynamicPropertyRegistry registry) {
+        System.out.println("container url: {}" + container.getReplicaSetUrl("users"));
+        System.out.println("container host/port: {}/{}" + container.getHost() + " - " + container.getFirstMappedPort());
+        registry.add("spring.data.mongodb.uri", () -> container.getReplicaSetUrl("users"));
+    }
 
     @Autowired
     private UserRepository userRepository;
-
-    UUID uuidUser1 = UUID.fromString("81099a9e-0d59-4571-a04c-31a08a711e3b");
-    UUID uuidUser2 = UUID.fromString("26977eee-89f8-11ec-a8a3-0242ac120003");
 
     @BeforeEach
     public void setUp() {
 
         userRepository.deleteAll().block();
+
+        UUID uuidUser1 = UUID.fromString("81099a9e-0d59-4571-a04c-31a08a711e3b");
+        UUID uuidUser2 = UUID.fromString("26977eee-89f8-11ec-a8a3-0242ac120003");
 
         UUID uuidGame1 = UUID.fromString("dcacb291-b4aa-4029-8e9b-284c8ca80296");
         UUID uuidGame2 = UUID.fromString("09fabe32-7362-4bfb-ac05-b7bf854c6e0f");
