24ffefc9ff59d50347505979397d53c65eb7630e
albertmart000 <albertmart000@yahoo.es>
2023-10-17 17:21:35 +0200

    Add ChallengeDocumentToDtoConverterTest and ResourceHelperTest

---
+++ betsanddice-user/src/test/java/com/betsanddice/user/helper/ResourceHelperTest.java
@@ -0,0 +1,24 @@
+package com.betsanddice.user.helper;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ResourceHelperTest {
+
+    @Test
+    @DisplayName("Read a resource as String test")
+    void readResourceAsStringTest (){
+        ResourceHelper resourceHelper = new ResourceHelper("json/random.json");
+        String expected = "{\"name\": \"RandomName\", \"num\": [1,2,3]}";
+        assertEquals(expected, resourceHelper.readResourceAsString().get());
+    }
+
+    @Test
+    void failedReadResourceTest () {
+        String invalidPath = "somename/some.json";
+        ResourceHelper resourceHelper = new ResourceHelper(invalidPath);
+        assertEquals(null, resourceHelper.readResourceAsString());
+    }
+}
