ea3bb1ab51f118f9083411b7be32ab2f2722db20
albertmart000 <albertmart000@yahoo.es>
2023-10-19 18:05:09 +0200

    Tunnig micro bet and micro stat

--- betsanddice-stat/src/main/java/com/bestanddice/stat/App.java
+++
@@ -1,9 +0,0 @@
-package com.bestanddice.stat;
-
-import org.springframework.boot.SpringApplication;
-
-public class App {
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
-}
