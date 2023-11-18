4eea5f350c294b934c59a0fa613157a9197d65ac
albertmart000 <albertmart000@yahoo.es>
2023-11-18 10:05:11 +0100

    Added CrapsGameControllerTest and CrapsGameIntegrationTest

--- betsanddice-craps/src/test/java/com/betsanddice/craps/integration/CrapsGameIntegrationTest.java
+++ betsanddice-craps/src/test/java/com/betsanddice/craps/integration/CrapsGameIntegrationTest.java
@@ -18,6 +18,7 @@ import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -31,6 +32,8 @@ import static org.hamcrest.Matchers.equalTo;
 @Testcontainers
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class CrapsGameIntegrationTest {
+
+    private final String CRAPS_BASE_URL = "/betsanddice/api/v1/craps";
 
     @Container
     static MongoDBContainer container = new MongoDBContainer("mongo")
@@ -50,26 +53,26 @@ class CrapsGameIntegrationTest {
     @Autowired
     private CrapsGameRepository crapsGameRepository;
 
+    UUID uuidUser = UUID.fromString("706507d4-b89f-41eb-a7eb-41838d08a08f");
+
     UUID uuidCrapsGame1 = UUID.fromString("50feba3c-3cbf-48ad-8142-cccf7c6bf3d3");
     UUID uuidCrapsGame2 = UUID.fromString("6160a07c-1d0f-4ac0-80b0-ef8f17bcad53");
 
+    CrapsGameDocument crapsGameDocument1 = new CrapsGameDocument();
+    CrapsGameDocument crapsGameDocument2 = new CrapsGameDocument();
+
     @BeforeEach
     void setUp() {
-
         crapsGameRepository.deleteAll().block();
 
-        UUID uuidUser = UUID.fromString("706507d4-b89f-41eb-a7eb-41838d08a08f");
-
-        DiceRollDocument diceRollDocument1= new DiceRollDocument( 1, 2, 3);
-        DiceRollDocument diceRollDocument2= new DiceRollDocument( 3, 4, 7);
+        DiceRollDocument diceRollDocument1 = new DiceRollDocument(1, 2, 3);
+        DiceRollDocument diceRollDocument2 = new DiceRollDocument(3, 4, 7);
         List<DiceRollDocument> diceRollsList = List.of(diceRollDocument1, diceRollDocument2);
 
         LocalDateTime date = LocalDateTime.of(2023, 1, 31, 12, 0, 0);
 
-        CrapsGameDocument crapsGameDocument1 = new CrapsGameDocument(uuidCrapsGame1, uuidUser, date,
-                2, diceRollsList);
-        CrapsGameDocument crapsGameDocument2 = new CrapsGameDocument(uuidCrapsGame2, uuidUser, date,
-                2, diceRollsList);
+        crapsGameDocument1 = new CrapsGameDocument(uuidCrapsGame1, uuidUser, date, 2, diceRollsList);
+        crapsGameDocument2 = new CrapsGameDocument(uuidCrapsGame2, uuidUser, date, 2, diceRollsList);
 
         crapsGameRepository.saveAll(Flux.just(crapsGameDocument1, crapsGameDocument2)).blockLast();
     }
@@ -77,7 +80,7 @@ class CrapsGameIntegrationTest {
     @Test
     void test() {
         webTestClient.get()
-                .uri("/betsanddice/api/v1/craps/test")
+                .uri(CRAPS_BASE_URL + "/test")
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
                 .expectStatus().isOk()
@@ -88,11 +91,24 @@ class CrapsGameIntegrationTest {
     @Test
     void getAllCrapsGames_CrapsGamesExist_CrapsReturned() {
         webTestClient.get()
-                .uri("/betsanddice/api/v1/craps/crapsGames")
+                .uri(CRAPS_BASE_URL + "/crapsGames")
                 .exchange()
                 .expectStatus().isOk()
                 .expectBodyList(CrapsGameDto.class)
                 .hasSize(2);
+    }
+
+    @Test
+    void addCrapsGameToUserTest() {
+        webTestClient.post()
+                .uri(CRAPS_BASE_URL + "/crapsGames/{userId}", uuidUser)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(crapsGameDocument1), CrapsGameDocument.class)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody();
+                //.equals(Mono.just(crapsGameDocument1).block());
     }
 
 }
