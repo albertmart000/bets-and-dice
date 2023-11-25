880ff7078b532948ab3db0763b5c33099ffee112
albertmart000 <albertmart000@yahoo.es>
2023-11-25 20:59:15 +0100

    Added endpoint addGameTutorial in micro Tutorial

--- betsanddice-tutorial/src/main/java/com/betsanddice/tutorial/service/IGameTutorialService.java
+++ betsanddice-tutorial/src/main/java/com/betsanddice/tutorial/service/IGameTutorialService.java
@@ -1,10 +1,13 @@
 package com.betsanddice.tutorial.service;
 
-import com.betsanddice.tutorial.dto.GameTutorialDto;
+import com.betsanddice.tutorial.dto.in.GameTutorialDtoByName;
+import com.betsanddice.tutorial.dto.out.GameTutorialDto;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface IGameTutorialService {
+
+    Mono<GameTutorialDto> addGameTutorial(GameTutorialDtoByName gameTutorialDtoByName);
 
     Flux<GameTutorialDto> getAllGameTutorials();
 
