b25d15edba2df80d001cc61fe0e3bb4f9d46b647
albertmart000 <albertmart000@yahoo.es>
2024-02-12 11:00:41 +0100

    Configured stop_Project.sh

--- start_Project.sh
+++ start_Project.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
 # Build images micros
-
 directories=("betsanddice-user" "betsanddice-stat" "betsanddice-tutorial" "betsanddice-craps")
-
 for dir in "${directories[@]}"; do
     # shellcheck disable=SC2164
     (cd "$dir" && bash build_Docker.sh)
-
 done
 
 docker image prune -f
--- stop_Project.sh
+++ stop_Project.sh
@@ -1,23 +1,26 @@
+#!/bin/sh
+
+cd docker
+
 # Delete all images dangling
-docker image prune
-
-# Delete all images unused
-docker image prune --filter="label=unused"
-
-# Delete all images dangling and unused
-docker image prune -a
+docker image prune -f
 
 # Delete all volume dangling
-docker volume prune
+docker volume prune -f
 
-# Delete all
-docker system prune -a --volumes
+docker-compose down
 
-#WARNING! This will remove:
-#        - all stopped containers
-#        - all networks not used by at least one container
-#        - all anonymous volumes not used by at least one container
-#        - all images without at least one container associated to them
-#        - all build cache
-
-# To not prompt for confirmation add -f
+## Delete all images unused
+#docker image prune --filter="label=unused"
+## Delete all images dangling and unused
+#docker image prune -a
+## Delete all
+#docker system prune -a --volumes
+##WARNING! This will remove:
+##        - all stopped containers
+##        - all networks not used by at least one container
+##        - all anonymous volumes not used by at least one container
+##        - all images without at least one container associated to them
+##        - all build cache
+#
+## To not prompt for confirmation add -f
