84890fea9bec49f41c0ca9316a8921565841f7a5
albertmart000 <albertmart000@yahoo.es>
2024-02-21 17:39:45 +0100

    Added Consul Depencies and @EnableDiscoveryClient in user, tutorial, craps and stat

--- betsanddice-user/src/main/java/com/betsanddice/user/App.java
+++ betsanddice-user/src/main/java/com/betsanddice/user/App.java
@@ -2,7 +2,9 @@ package com.betsanddice.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
--- start_Project.sh
+++ start_Project.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#Run Consul:
+#docker run -d --name=consul-server -p 8500:8500 -p 8600:8600/udp consul:1.15.3 agent -server -node=server-1 -bootstrap-expect=1 -ui -bind 127.0.0.1 -client 0.0.0.0
+
 # Build images micros
 directories=("betsanddice-user" "betsanddice-stat" "betsanddice-tutorial" "betsanddice-craps")
 for dir in "${directories[@]}"; do
