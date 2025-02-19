cc816f1cbe0e5b405d6376a8431c4ddb44c3a362
albertmart000 <albertmart000@yahoo.es>
2025-02-19 07:58:00 +0100

    Added JasonPropertyOrder in UserDto

--- betsanddice-user/src/main/java/com/betsanddice/user/dto/UserDto.java
+++ betsanddice-user/src/main/java/com/betsanddice/user/dto/UserDto.java
@@ -3,6 +3,7 @@ package com.betsanddice.user.dto;
 import com.betsanddice.user.annotations.ValidUUID;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -24,38 +25,39 @@ import java.util.UUID;
 @Builder
 @Getter
 @Setter
+@JsonPropertyOrder({"user_id", "name", "surname", "birthdate", "nickname", "email", "password"})
 public class UserDto {
 
-    @JsonProperty(value = "user_id", index = 0)
+    @JsonProperty(value = "user_id")
     @ValidUUID(message = "Invalid ID format. Please indicate the correct format.")
     private UUID uuid;
 
-    @JsonProperty(value = "name", index = 1)
+    @JsonProperty(value = "name")
     @NotBlank(message = "Invalid Name: Name can't be empty.")
     @NotNull(message = "Invalid Name: Name can't be NULL.")
     @Size(min = 2, message = "Invalid Name: Name should have at least 2 characters.")
     private String name;
 
-    @JsonProperty(value = "surname", index = 2)
+    @JsonProperty(value = "surname")
     @NotBlank(message = "Invalid Surname: Surname can't be empty." )
     @NotNull(message = "Invalid Surname: Surname can't be NULL.")
     @Size(min = 2, message = "Invalid Surname: Surname should have at least 2 characters")
     private String surname;
 
-    @JsonProperty(value = "birthdate", index = 3)
+    @JsonProperty(value = "birthdate")
     private String birthdate;
 
-    @JsonProperty(value = "nickname", index = 4)
+    @JsonProperty(value = "nickname")
     @NotBlank(message = "Invalid Nickname: Surname can't be empty." )
     @NotNull(message = "Invalid Nickname: Surname can't be NULL.")
     @Size(min = 2, message = "Invalid Nickname: Nickname should have at least 2 characters")
     private String nickname;
 
-    @JsonProperty(value = "email", index = 5)
+    @JsonProperty(value = "email")
     @Email
     private String email;
 
-    @JsonProperty(value = "password", index = 6)
+    @JsonProperty(value = "password")
     @NotBlank(message = "Invalid Password: Password can't be empty." )
     @NotNull(message = "Invalid Password: Password can't be NULL.")
     @Size(min = 2, message = "Invalid Password: Password should have at least 2 characters")
