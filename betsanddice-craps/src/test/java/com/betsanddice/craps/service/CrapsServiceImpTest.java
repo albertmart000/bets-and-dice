38f08a9369fc92bf899309831905a9d9244c6b96
albertmart000 <albertmart000@yahoo.es>
2025-02-25 08:48:34 +0100

    Added @SuppressWarnings("unchecked") in CrapsServiceImpTest

--- betsanddice-craps/src/test/java/com/betsanddice/craps/service/CrapsServiceImpTest.java
+++ betsanddice-craps/src/test/java/com/betsanddice/craps/service/CrapsServiceImpTest.java
@@ -24,6 +24,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+@SuppressWarnings("unchecked")
 class CrapsServiceImpTest {
 
     @Mock
