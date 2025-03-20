86ea18a4d4ead8a1081431511682401019237403
albertmart000 <albertmart000@yahoo.es>
2025-03-20 20:28:34 +0100

    Added micro betsanddice-eureka

---
+++ betsanddice-eureka/src/test/java/com/betsanddice/betsanddice_eureka/BetsanddiceEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betsanddice.betsanddice_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetsanddiceEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
