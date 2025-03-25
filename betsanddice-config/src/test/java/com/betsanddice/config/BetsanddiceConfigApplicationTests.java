978395036fc6dc9aa50774fdc405e7bc1580efcc
albertmart000 <albertmart000@yahoo.es>
2025-03-25 19:17:33 +0100

    Added micro Betsanddice-config

---
+++ betsanddice-config/src/test/java/com/betsanddice/config/BetsanddiceConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betsanddice.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetsanddiceConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
