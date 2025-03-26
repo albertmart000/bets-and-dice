aacf271f8679f7fedb7920694f99f763c9eb611a
albertmart000 <albertmart000@yahoo.es>
2025-03-26 16:31:27 +0100

    Connected micro betsanddice-craps with micro betsanddice-eureka

--- betsanddice-user/src/test/java/com/betsanddice/user/controller/UserControllerTest.java
+++ betsanddice-user/src/test/java/com/betsanddice/user/controller/UserControllerTest.java
@@ -8,8 +8,8 @@ import com.betsanddice.user.service.client.ICrapsGameClientService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
@@ -23,10 +23,10 @@ class UserControllerTest {
     @Autowired
     private WebTestClient webTestClient;
 
-    @MockBean
+    @MockitoBean
     private IUserService userService;
 
-    @MockBean
+    @MockitoBean
     private ICrapsGameClientService crapsGameClientService;
 
     UserDto userDto1 = new UserDto();
