63937f5f1748f06907ccde0c66366e665d46c300
albertmart000 <albertmart000@yahoo.es>
2025-04-06 14:17:22 +0200

    Reactored build_Docker.sh files

--- betsanddice-eureka/build_Docker.sh
+++ betsanddice-eureka/build_Docker.sh
@@ -1,25 +1,15 @@
 #!/bin/sh
 
-fileConfig=config/.env.dev
-now=$(date +'%d-%m-%Y %H:%M:%S:%3N')
-base_dir=$(pwd)
+fileConfig=../config/.env.dev
+source "$fileConfig"
 
-if [ -f "$fileConfig" ]; then
-  while IFS='=' read -r key value; do
-    key=$(echo "$key" | tr '.' '_')
-    export "${key}=${value}"
-  done < "$fileConfig"
+echo "Date: $(date +'%d-%m-%Y %H:%M:%S:%3N')"
+echo "REGISTRY_NAME: $REGISTRY_NAME"
 
-  echo "Date: $now"
-  echo "REGISTRY_NAME: $REGISTRY_NAME"
+./gradlew :betsanddice-eureka:clean && ./gradlew :betsanddice-eureka:build
 
-  ./gradlew :betsanddice-eureka:clean && ./gradlew :betsanddice-eureka:build
+docker build -t "$REGISTRY_NAME:betsanddice-eureka-$EUREKA_TAG" .
 
-  docker build -t "$REGISTRY_NAME:betsanddice-eureka-$EUREKA_TAG" .
-
-  if [ "$ENV" = "dev" ]; then
-    docker push "$REGISTRY_NAME:betsanddice-eureka-$EUREKA_TAG"
-  fi
-else
-  echo "$fileConfig not found."
+if [ "$ENV" = "dev" ]; then
+  docker push "$REGISTRY_NAME:betsanddice-eureka-$EUREKA_TAG"
 fi
